45c8f68bca7248080c5c7e637528bfeb00c79eb2
迎舟 魏 <yingzhou.wei@shuyun.com>
2019-06-20 15:24:04 +0800

    调整pom

--- sf4j-log/src/main/java/com/ywei/spi/service/sf4j/Sf4jLogServiceImpl.java
+++ sf4j-log/src/main/java/com/ywei/spi/service/sf4j/Sf4jLogServiceImpl.java
@@ -1,9 +1,27 @@
 package com.ywei.spi.service.sf4j;
 
+import com.ywei.spi.service.ILogService;
+
 /**
  * @author yingzhou.wei
  * @Description TODO
  * @date 2019/6/20
  */
-public class Sf4jLogServiceImpl {
+public class Sf4jLogServiceImpl implements ILogService {
+    @Override
+    public void debug(String message) {
+        System.out.println(" sf4j debug msg: " + message);
+    }
+    @Override
+    public void info(String message) {
+        System.out.println(" sf4j info msg: " + message);
+    }
+    @Override
+    public void error(String message) {
+        System.out.println(" sf4j error msg: " + message);
+    }
+    @Override
+    public void warn(String message) {
+        System.out.println(" sf4j warn msg: " + message);
+    }
 }
